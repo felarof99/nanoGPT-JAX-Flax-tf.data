b574da05d149661be12c95b431d5f584fbe0f8b7
felarof99 <nithin.sonti@gmail.com>
2024-03-18 16:49:15 +0000

    updated model.py from single-head-attention notebook

--- model.py
+++ model.py
@@ -10,15 +10,75 @@ import tensorflow as tf
 import pdb
 import functools
 
+class SingleHeadAttention(nn.Module):
+  head_size: int
+  T: int
+
+  def setup(self):
+    self.key_layer = nn.Dense(self.head_size, use_bias=False)
+    self.query_layer = nn.Dense(self.head_size, use_bias=False)
+    self.value_layer = nn.Dense(self.head_size, use_bias=False)
+    self.dropout = nn.Dropout(rate=0.2)
+
+  
+  def __call__(self, tokens: jnp.array, training: bool):
+    """Tokens, each with some channel dim. Ex: [ [0.1, 0.2], [0.3, 0.4] ]"""
+    mask = jnp.tril(jnp.ones(shape=(self.T, self.T))) # jnp.ones(shape=(self.T, self.T))
+
+    # input: (T, channels)
+    # output: (T, head_size)
+    keys = self.key_layer(tokens)
+    queries = self.query_layer(tokens)
+    values = self.value_layer(tokens)
+
+    # chanel info size
+    C = int(tokens.shape[-1])
+
+    # compute attention score.
+    wei = jnp.dot(queries, keys.T) * C**0.5 # (T, head_size) * (head_size, T) == (T, T)
+    wei = jnp.where(mask==0, -jnp.inf, wei)
+    wei = nn.softmax(wei, axis=-1)
+
+    attention_values = jnp.dot(wei, values) # (T, T) * (T, head_size))
+    attention_values = self.dropout(attention_values, deterministic=not training)
+    return attention_values # (T, head_size)
+
+
+class MultiHeadAttention(nn.Module):
+  num_heads: int
+  head_size: int # head_size * num_heads is the final embedding dimension you get, after concatenating from all heads
+  T: int
+
+  def setup(self):
+    self.heads = [
+        SingleHeadAttention(head_size=self.head_size, T=self.T) for _ in range(self.num_heads)
+    ]
+    final_output_size = self.num_heads * self.head_size
+    self.projection = nn.Dense(features=final_output_size)
+
+    self.dropout = nn.Dropout(rate=0.2)
+
+  def __call__(self, tokens: jnp.array, training: bool):
+    output_from_each_head = []
+    for h in self.heads:
+      output = h(tokens, training)
+      output_from_each_head.append(output)
+
+    # Run multiple attention heads in parallel and concatenate
+    # their output along channel dimension, i.e., dim==-1
+    out_from_all_heads = jnp.concatenate(output_from_each_head, axis=-1)
+
+    projection =  self.projection(out_from_all_heads)
+
+    return self.dropout(projection, deterministic=not training)
+
 class FeedForward(nn.Module):
   output_size: int
 
   def setup(self):
-    # attention paper uses 4 times token_info_size when doing linear transformation.
+    # Attention paper uses 4 times token_info_size when doing linear transformation
     # and then projects it back to token_info_size in linear transformation layer.
     self.ffwd = nn.Dense(features=4 * self.output_size)
-
-    # projection layer, which goes back into residual pathway.
     self.projection = nn.Dense(self.output_size)
 
   def __call__(self, x, training: bool):
@@ -26,90 +86,21 @@ class FeedForward(nn.Module):
     x = self.projection(x)
     return x
 
-
-class Head(nn.Module):
-  token_info_size: int # head_size; how much (emb dim) info each token emits for keys, queries, values.
-  T: int # block size; number of tokens in a block
-
-  def setup(self):
-    # key, query will take vector of size C.
-    # i.e., channels containing info of token and will output token_info_size
-    self.key_layer = nn.Dense(self.token_info_size, use_bias=False)
-    self.query_layer = nn.Dense(self.token_info_size, use_bias=False)
-    self.value_layer = nn.Dense(self.token_info_size, use_bias=False)
-
-    self.dropout = nn.Dropout(rate=0.2)
-
-
-  def __call__(self, block_of_tokens_with_info_channels: jnp.array, training: bool):
-    """Accepts a block of tokens with info channels, like (8, 65)."""
-
-    # TODO(ntnsonti): Double check; but tril should not be learnable according cGPT.
-    tril = jnp.tril(jnp.ones(shape=(self.T, self.T)))
-
-    # input: (T, info channels)
-    # output: (T, token_info_size)
-    keys = self.key_layer(block_of_tokens_with_info_channels)
-    queries = self.query_layer(block_of_tokens_with_info_channels)
-    values = self.value_layer(block_of_tokens_with_info_channels)
-
-    # chanel info size
-    C = int(block_of_tokens_with_info_channels.shape[-1])
-    # print("[ntn99] channel_info_size: ", C)
-
-    # compute attention score.
-    wei = jnp.dot(queries, keys.T) * C**0.5 # (T, token_info_size) * (token_info_size, T) == (T, T)
-    wei = jnp.where(tril==0, -jnp.inf, wei)
-    wei = nn.softmax(wei, axis=-1)
-
-    attention_values = jnp.dot(wei, values) # (T, T) * (T, token_info_size))
-
-    attention_values = self.dropout(attention_values, deterministic=not training)
-
-    return attention_values # (T, token_info_size)
-
-
-class MultiHeadAttention(nn.Module):
+class TransformerEncoderBlock(nn.Module):
   num_heads: int
-  final_token_info_size: int # After concatenating from all heads, how much info (values -- emb size) you have on each token.
-  T: int
-
-  def setup(self):
-    self.token_info_size_per_head = int(self.final_token_info_size/self.num_heads)
-    self.heads = [
-        Head(token_info_size=self.token_info_size_per_head, T=self.T) for _ in range(self.num_heads)
-    ]
-
-    self.projection = nn.Dense(features=self.final_token_info_size)
-
-    self.dropout = nn.Dropout(rate=0.2)
-
-  def __call__(self, block_of_tokens_with_info_channels: jnp.array, training: bool):
-    out_from_each_head = jnp.array([h(block_of_tokens_with_info_channels, training) for h in self.heads])
-
-    # You just run multiple attention heads in parallel and concatenate
-    # their output along channel dimension, i.e., dim==-1
-    out_from_all_heads = jnp.concatenate(out_from_each_head, axis=-1)
-    # print("[ntn99] out_from_all_heads concatenated shape: ", out_from_all_heads.shape)
-
-    projection =  self.projection(out_from_all_heads)
-
-    return self.dropout(projection, deterministic=not training)
-
-
-class Block(nn.Module):
-  num_heads: int
-  final_token_info_size: int
+  # output_size == head_size * num_heads, is the final embedding dimension you get after concatenating from all heads.
+  output_size: int
   T: int
 
   def setup(self):
     # communication.
+    self.head_size = self.output_size // self.num_heads  # each SingleAttentionHead will produce head_size worth of info for key, value, querie. You concatenate all of them to get the final output_size.
     self.self_attention_heads = MultiHeadAttention(num_heads=self.num_heads,
-                                                   final_token_info_size=self.final_token_info_size,
+                                                   head_size = self.head_size,
                                                    T=self.T)
 
     # computation.
-    self.computation_layer = FeedForward(output_size=self.final_token_info_size)
+    self.computation_layer = FeedForward(output_size=self.output_size)
 
     self.ln1 = nn.LayerNorm()
     self.ln2 = nn.LayerNorm()
@@ -117,13 +108,13 @@ class Block(nn.Module):
     self.dropout = nn.Dropout(rate=0.2)
 
   def __call__(self, x, training: bool):
+    # transformer encoder forward pass
     x = x + self.self_attention_heads(self.ln1(x), training)
 
     x = x + self.computation_layer(self.ln2(x), training)
 
     x = self.dropout(x, deterministic=not training)
     return x
-
 
 class LanguageModel(nn.Module):
   """Reads one char and predicits the next char."""
@@ -143,19 +134,21 @@ class LanguageModel(nn.Module):
     # Concantenate token_info from all 4 heards, gives us 32
     self.num_blocks = 4
     self.blocks = [
-        Block(num_heads=4, final_token_info_size=self.n_embed, T=self.T) for _ in range(self.num_blocks)
+        TransformerEncoderBlock(num_heads=4,
+                                output_size=self.n_embed,
+                                T=self.T) for _ in range(self.num_blocks)
     ]
     self.ln = nn.LayerNorm()
     self.lang_model_head = nn.Dense(features=self.C)
 
   def __call__(self, block_of_tokens: jnp.array, training: bool):
     """Accepts a block of tokens, like [0, 1, 2, 3, 4, 5, 6, 7]."""
-
-    # generate em for each token. output: (T, n_embed)
+    # generate emb for each token. output: (T, n_embed)
     token_embs = self.token_embedding_table(block_of_tokens)
 
     # generate position embs for each token.
-    num_pos = self.T
+    # num_pos = block_of_tokens.shape[0]
+    num_pos = T
     positions = jnp.arange(0, num_pos)
     pos_embs = self.pos_embedding_table(positions)
 
@@ -163,6 +156,7 @@ class LanguageModel(nn.Module):
     x = token_embs + pos_embs
 
     # feed x into self-attention head.
+    ## language model, forward pass, block_of_tokens
     for i in range(self.num_blocks):
       x = self.blocks[i](x, training)
 
